c999561c294e2f0fe6d0e1081f6e0d68f3730501
Balintescu <andrei.balintescu@stud.ubbcluj.ro>
2024-10-12 13:46:44 +0300

    Added optimizations for Subtasks 1 and 2, completed Subtask 3

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,16 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Grades grades = new Grades();
-        int[] array = {25, 15, 80, 45};
+        int[] array = {29, 37, 38, 41, 84, 67};
         int[] inssuficientGrades = grades.getInsufficientGrades(array);
         System.out.print("Noten:");
         System.out.println(Arrays.toString(array));
         System.out.print("Nicht ausreichende Noten:");
         System.out.println(Arrays.toString(inssuficientGrades));
         double average = grades.getAverage(array);
-        System.out.println("Durchschnittswert: " + average);
+        String fixedAverage = String.format("%.2f", average);
+        System.out.println("Durchschnittswert: " + fixedAverage);
+        System.out.println("Abgerundete Noten:" + Arrays.toString(grades.getRoundedGrades(array)));
+
     }
 }
--- src/Task1/Grades.java
+++ src/Task1/Grades.java
@@ -1,21 +1,48 @@
 package Task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Grades {
     public int[] getInsufficientGrades(int[] grades) {
-        int[] insufficientGrades = new int[grades.length];
-        for (int i = 0; i < grades.length; i++) {
-            if (grades[i] < 40)
-                insufficientGrades[i] = grades[i];
-
-
+        List<Integer> insufficientGradesList = new ArrayList<>();
+        for (int grade : grades) {
+            if (grade < 40) {
+                insufficientGradesList.add(grade);
+            }
         }
+
+        int[] insufficientGrades = new int[insufficientGradesList.size()];
+        for (int i = 0; i < insufficientGradesList.size(); i++) {
+            insufficientGrades[i] = insufficientGradesList.get(i);
+        }
+
         return insufficientGrades;
     }
+
     public double getAverage(int[] grades) {
         int sum = 0;
         for (int grade : grades) {
             sum += grade;
         }
         return (double) sum / grades.length;
+    }
+
+    public int[] getRoundedGrades(int[] grades) {
+        int[] roundedGrades = new int[grades.length];
+
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] >= 38) {
+                int roundedGrade = ((grades[i] / 5) + 1) * 5;
+                if (roundedGrade - grades[i] < 3)
+                    roundedGrades[i] = roundedGrade;
+                else
+                    roundedGrades[i] = grades[i];
+            }
+            else
+                roundedGrades[i] = grades[i];
+
+        }
+        return roundedGrades;
     }
 }
